e50486d22d06365d5df6b818692223d34bf76fec
lau <lau.bannenberg@gmail.com>
2024-08-31 10:35:57 +0200

    Implement passing boolean HTML attributes

--- src/Model/BrickAttributesBag.php
+++ src/Model/BrickAttributesBag.php
@@ -5,9 +5,16 @@ namespace Corrivate\LayoutBricks\Model;
 
 class BrickAttributesBag
 {
+    public const HTML_BOOLEAN_ATTRIBUTES = [ // sourced from https://meiert.com/en/blog/boolean-attributes-of-html/
+        'allowfullscreen', 'async', 'autofocus', 'autoplay', 'checked', 'controls', 'default', 'defer', 'disabled',
+        'formnovalidate', 'inert', 'ismap', 'itemscope', 'loop', 'multiple', 'muted', 'nomodule', 'novalidate', 'open',
+        'readonly', 'required', 'reversed', 'selected'
+    ];
+    public const HTML_APPENDABLE_ATTRIBUTES = ['class', 'style'];
+
     public function __construct(
-        private array $attributes = [])
-    {
+        private array $attributes = []
+    ) {
     }
 
     public function merge($defaultAttributes = []): static
@@ -17,13 +24,20 @@ class BrickAttributesBag
 
     public function toHtml(): string
     {
-        return (string)$this;
+        return (string) $this;
     }
 
-    public function __toString(){
+    public function __toString()
+    {
         $output = [];
-        foreach($this->attributes as $key => $value) {
-            $output[] = $key . '="' . $value . '"';
+        foreach ($this->attributes as $key => $value) {
+            if (in_array($key, self::HTML_BOOLEAN_ATTRIBUTES)) {
+                if ($value) {
+                    $output[] = $key.'="'.$key.'"'; // We map for example ['checked' => true] to checked="checked"
+                }
+            } else {
+                $output[] = $key.'="'.$value.'"';
+            }
         }
         return implode(' ', $output);
     }
