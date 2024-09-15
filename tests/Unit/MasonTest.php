b648dba263e58589a15579dbe38c38671670625c
lau <lau.bannenberg@gmail.com>
2024-09-15 20:15:45 +0200

    Add more $mason tests

--- tests/Unit/MasonTest.php
+++ tests/Unit/MasonTest.php
@@ -35,4 +35,50 @@ class MasonTest extends TestCase
         // Our mocks have received expected method calls
     }
 
+
+    public function testThatItThrowsWhenGivenAnUnknownAlias(){
+        // ARRANGE
+        $layout = $this->createMock(Layout::class);
+
+
+        /** @var Layout $layout */
+        $mason = new Mason(
+            $layout,
+            [] // No aliases configured
+        );
+
+        $this->expectException(\InvalidArgumentException::class);
+
+        // ACT
+        $mason('elements.link.external');
+
+        // ASSERT
+        // The exception should have been thrown
+    }
+
+
+    public function testItDoesNotTreatAMagentoPathAsAlias(){
+        // ARRANGE
+        $layout = $this->createMock(Layout::class);
+        $block = $this->createMock(\Magento\Framework\View\Element\Template::class);
+
+        $layout->expects($this->once())->method('createBlock')->with(\Magento\Framework\View\Element\Template::class)->willReturn($block);
+        $block->expects($this->once())->method('setTemplate')->with('Corrivate_LayoutBricks::elements/link/external.phtml')->willReturn($block);
+        $block->expects($this->atLeastOnce())->method('setData')->willReturn($block);
+        $block->expects($this->atLeastOnce())->method('toHtml')->willReturn('html');
+
+
+        /** @var Layout $layout */
+        $mason = new Mason(
+            $layout,
+            [] // No aliases configured
+        );
+
+        // ACT
+        $mason('Corrivate_LayoutBricks::elements/link/external.phtml');
+
+        // ASSERT
+        // The exception should have been thrown
+    }
+
 }
